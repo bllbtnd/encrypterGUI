e0cb74dc156867bcac0ed80283450a5f7c584b4b
Balla Botond <ballabotond@protonmail.com>
2022-11-23 18:44:11 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ eod = "e"
 key = None
 showpasswordbool = False
 frame = tk.Tk()
+
 def passhandler():
     password = password_provided.encode()
     salt = b'salt'
@@ -60,8 +61,16 @@ def main():
         else:
             pw.config(show="*")
             showpasswordbool = False
+    def info():
+        frame2 = tk.Tk()
+        frame2.title("Informations")
+        frame2.config(padx=20, pady=20)
+        infolabel = tk.Label(frame2, text="This program was made by\nBalla Botond.")
+        infolabel.pack()
+        frame2.mainloop()
 
     frame.title("Basilisk")
+    frame.config(padx=10, pady=10)
     frame.resizable(False, False)
     photo = tk.PhotoImage(file='ico.png')
     frame.wm_iconphoto(False, photo)
@@ -71,18 +80,20 @@ def main():
     passwordlabel = tk.Label(frame, height=1, text="Password:")
     fname = tk.Entry(frame, width=20)
     showbutton = tk.Button(frame, height=1, text="👁️", command=showpw)
+    infobutton = tk.Button(frame, height=1, text="i", command=info)
     pw = tk.Entry(frame, show="*", width=20)
     encodebutton.grid(row=0, column=0, columnspan=4)
     filenamelabel.grid(row=1, column=0)
-    fname.grid(row=1, column=1, columnspan=3)
+    fname.grid(row=1, column=1, columnspan=3, pady=5)
     passwordlabel.grid(row=2, column=0, ipadx=10)
-    pw.grid(row=2, column=1, columnspan=3)
+    pw.grid(row=2, column=1, columnspan=3, pady=5)
+    infobutton.grid(row=3, column=0, sticky="W")
     def savethevalues():
         global filename, password_provided
         filename = fname.get()
         password_provided = pw.get()
         passhandler()
-    button = tk.Button(frame, text="Encrypt/Decript", command=savethevalues)
+    button = tk.Button(frame, text="Go!", command=savethevalues)
     button.grid(row=3, column=0, columnspan=4)
     showbutton.grid(row=3, column=3, sticky="E")
     frame.mainloop()
